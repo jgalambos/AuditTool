7eb9a6cc943963eef6224372a148d6d13248bd5d
jgalambos <theunbeliever@gmail.com>
2018-08-21 15:29:40 -0400

    Building Tree based on files

--- AuditTool/Form1.cs
+++ AuditTool/Form1.cs
@@ -28,9 +28,47 @@ namespace AuditTool {
         private void button2_Click(object sender, EventArgs e) {
             LabelReadout.Text = "";
             TreeViewMain.Nodes.Clear();
-            TreeNode RootNode = new TreeNode(@"C:\Repositories");
+            TreeNode RootNode = new TreeNode(@"C:\");
+            GetFileStructure(RootNode);
             TreeViewMain.Nodes.Add(RootNode);
-            LabelReadout.Text = GetDirectoriesAndFiles(@"C:\Repositories");
+            // LabelReadout.Text = GetDirectoriesAndFiles(@"C:\Repositories");
+        }
+
+        private void GetFileStructure(TreeNode root) {
+            List<TreeNode> TreeNodes = new List<TreeNode>();
+            DirectoryInfo info = new DirectoryInfo(root.Text);
+            /*FileInfo[] files = info.GetFiles().OrderBy(p => p.LastAccessTime).ToArray();
+            foreach (FileInfo file in files) {
+                TreeNodes.Add(new TreeNode(file.FullName));
+            }*/
+            /*foreach (DirectoryInfo dir in info.GetDirectories()) {
+                GetFileStructureRecursive(root);
+                TreeNodes.Add(new TreeNode(dir.FullName));
+            }*/
+            GetFileStructureRecursive(root);
+            root.Nodes.AddRange(TreeNodes.ToArray());
+            TreeViewMain.Nodes.AddRange(TreeNodes.ToArray());
+        }
+
+        private void GetFileStructureRecursive(TreeNode parent) {
+            TreeNode value = new TreeNode();
+            List<TreeNode> TreeNodes = new List<TreeNode>();
+            DirectoryInfo info = new DirectoryInfo(parent.Text);
+            /*FileInfo[] files = info.GetFiles().OrderBy(p => p.LastAccessTime).ToArray();
+            foreach (FileInfo file in files) {
+                TreeNodes.Add(new TreeNode(file.FullName));
+            }*/
+            try {
+                foreach (DirectoryInfo dir in info.GetDirectories()) {
+                    value = new TreeNode(dir.FullName);
+                    GetFileStructureRecursive(value);
+                    TreeNodes.Add(value);
+                }
+                parent.Nodes.AddRange(TreeNodes.ToArray());
+            } catch (UnauthorizedAccessException ex) {
+                LabelReadout.Text += ex.Message + Environment.NewLine;
+            }
+
         }
 
         private string GetDirectoriesAndFiles(string path) {
