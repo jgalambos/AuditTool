5768b348ce0b1c73e49c2d52ac896e0b1c21185e
jgalambos <JG6XV@hscs.Virginia.edu>
2018-08-21 12:53:53 -0400

    drive types

--- AuditTool/Form1.cs
+++ AuditTool/Form1.cs
@@ -7,11 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AuditTool {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            DriveInfo[] readout = DriveInfo.GetDrives();
+            LabelReadout.Text = "";
+            foreach (DriveInfo i in readout) {
+                if (i.DriveType == DriveType.Fixed || i.DriveType == DriveType.Removable) {
+                    LabelReadout.Text += i.ToString() + " ";
+                }
+            }
         }
     }
 }
